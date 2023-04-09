89f3a1f3e3b1a18a92e5df5b2000d522c6bdc68c
Swapnil Patil <swapnil@Swapnils-MacBook-Pro.local>
2023-04-09 21:37:11 +0530

    work on cheque

---
+++ models/cheque.go
@@ -0,0 +1,75 @@
+package models
+
+import (
+	"fmt"
+	"swapnil-ex/models/db"
+	"time"
+	"gorm.io/gorm"
+)
+
+type Cheque struct {
+	ID            					uint    `json:"id"`
+	BankName     						string `json:"bank_name"`
+	IsCleared 							bool `json:"is_cleared"`
+	Amount       						float64 `json:"amount"`
+	TransactionID 					uint `json:"transaction_id"`
+	Date  									time.Time
+	CreatedAt 							time.Time
+	UpdatedAt 							time.Time
+  DeletedAt 							gorm.DeletedAt `gorm:"index"`
+}
+
+
+func migrateCheque() {
+	fmt.Println("migrating Transaction..")
+	err := db.Driver.AutoMigrate(&Cheque{})
+	if err != nil {
+		panic("failed to migrate database")
+	}
+}
+
+func NewCheque(chequeData map[string]interface{}) *Cheque {
+	cheque := &Cheque{}
+	cheque.Assign(chequeData)
+	return cheque
+}
+
+func (c *Cheque) Validate() error {
+	return nil
+}
+
+func (c *Cheque) Assign(chequeData map[string]interface{}) {
+	fmt.Printf("%+v\n", chequeData)
+	if bankName, ok := chequeData["bank_name"]; ok {
+		c.BankName = bankName.(string)
+	}
+}
+
+func (c *Cheque) All() ([]Cheque, error) {
+	var cheques []Cheque
+	err := db.Driver.Find(&cheques).Error
+	return cheques, err
+}
+
+func (c *Cheque) Find() error {
+	err := db.Driver.First(c, "ID = ?", c.ID).Error
+	return err
+}
+
+func (c *Cheque) Create() error {
+	err := db.Driver.Create(c).Error
+	db.Commit()
+	return err
+}
+
+func (c *Cheque) Update() error {
+	err := db.Driver.Save(c).Error
+	db.Commit()
+	return err
+}
+
+func (c *Cheque) Delete() error {
+	err := db.Driver.Delete(c).Error
+	db.Commit()
+	return err
+}

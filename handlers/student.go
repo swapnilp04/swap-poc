3b39c752d99cb9ab217f5ecdc5ff0a13293f24f3
Swapnil Patil <swapnil@Swapnils-MacBook-Pro.local>
2023-04-22 22:02:32 +0530

    work on UI

--- handlers/student.go
+++ handlers/student.go
@@ -84,6 +84,9 @@ func UpdateStudent(c echo.Context) error {
 	}
 
 	s.Assign(studentData)
+	if err := s.Validate(); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{"error": err.Error()})
+	}
 	if err := s.Update(); err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": swapErr.ErrInternalServer.Error()})
 	}
